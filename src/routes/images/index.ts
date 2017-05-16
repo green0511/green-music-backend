7ea0066ead2bb54a49fb32341676e3e13b76f43e
green <kmsbbb@163.com>
2017-05-16 18:03:41 +0800

    add type validate for image route

--- src/routes/images/index.ts
+++ src/routes/images/index.ts
@@ -9,6 +9,8 @@ import { getUploadedFiles, getUploadMiddleWare } from './uploader'
 import { IUser } from '../user'
 import { auth } from '../../auth'
 
+let availableTypes = ['covers', 'avatars']
+
 class ImageRoute {
   router: Router
 
@@ -21,6 +23,9 @@ class ImageRoute {
   // 上传
   public async upload(req: Request, res: Response, next: NextFunction) {
     let {type} = req.params
+    if (availableTypes.indexOf(type) == -1) {
+      return res.json({success: false, msg: '不支持此目录类型'}) 
+    }
     let user: IUser = req.user
 
     let middleWare = getUploadMiddleWare(type)
@@ -33,13 +38,16 @@ class ImageRoute {
         }
         return res.json({success: false, msg})
       }
-      serverDebugger('uploaded file: ', req.file)
-      return res.json({success: true, msg: '上传成功', data: { file: req.file.filename }})
+      serverDebugger('uploaded file: ', req['file'])
+      return res.json({success: true, msg: '上传成功', data: { file: req['file'].filename }})
     })
   }
 
   public getAllUrl(req: Request, res: Response) {
     let {type} = req.params
+    if (availableTypes.indexOf(type) == -1) {
+      return res.json({success: false, msg: '不支持此目录类型'}) 
+    }
     let host = req.headers.host
     serverDebugger('host: ', host)
     let prefix = 'http://' + host + '/static/' + type +'/'
